24a2db7046ac8439c6c3ca34698584cead17db0a
Prabhath-W <prabhath@hanikata.com>
2024-05-12 14:41:07 +0530

    Repo init

---
+++ Chinook/Models/Artist.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Chinook.Models
+{
+    public partial class Artist
+    {
+        public Artist()
+        {
+            Albums = new HashSet<Album>();
+        }
+
+        public long ArtistId { get; set; }
+        public string? Name { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
